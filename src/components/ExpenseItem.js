6808f3382d93f62f0c0d2b2f94845286014caecf
meauxrobert <153924537+meauxrobert@users.noreply.github.com>
2024-01-11 21:10:58 -0600

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,93 +1,128 @@
 import React, { useContext } from 'react';
+import { FaPlusCircle } from 'react-icons/fa';
+import { FaMinusCircle } from 'react-icons/fa';
+import { FaTimesCircle } from 'react-icons/fa';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 
 
 
 
+
+
+
+
 const ExpenseItem = (props) => {
-    const { dispatch, currency } = useContext(AppContext);
-
-
-
-
-    const handleDeleteExpense = () => {
-        dispatch({
-            type: 'DELETE_EXPENSE',
-            payload: props.id,
-        });
-    };
-
-
-
-
-    const increaseAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: 10,
-        };
-
-
-
-
-        dispatch({
-            type: 'ADD_EXPENSE',
-            payload: expense
-        });
-
-
-
-
-    }
-
-
-
-
-    const decreaseAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: 10,
-        };
-
-
-        dispatch({
-            type: 'RED_EXPENSE',
-            payload: expense
-        });
-
-
-
-
-    }
+   const { dispatch, currency } = useContext(AppContext);
 
 
 
 
 
 
-    return (
-        <tr>
-        <td>{props.name}</td>
-        <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><button onClick={event=> decreaseAllocation(props.name)}>-</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
-        </tr>
-    );
+
+
+   const handleDeleteExpense = () => {
+       dispatch({
+           type: 'DELETE_EXPENSE',
+           payload: props.id,
+       });
+   };
+
+
+
+
+
+
+
+
+   const increaseAllocation = (name) => {
+       const expense = {
+           name: name,
+           cost: 10,
+       };
+
+
+
+
+
+
+
+
+       dispatch({
+           type: 'ADD_EXPENSE',
+           payload: expense
+       });
+
+
+
+
+
+
+
+
+   }
+
+
+
+
+
+
+
+
+   const decreaseAllocation = (name) => {
+       const expense = {
+           name: name,
+           cost: 10,
+       };
+
+
+
+
+       dispatch({
+           type: 'RED_EXPENSE',
+           payload: expense
+       });
+
+
+
+
+
+
+
+
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+   return (
+       <tr>
+       <td>{props.name}</td>
+       <td>{currency}{props.cost}</td>
+       <td><FaPlusCircle size='1.5em' color="green" onClick={event=> increaseAllocation(props.name)}></FaPlusCircle></td>
+       <td><FaMinusCircle size='1.5em' color="red" onClick={event=> decreaseAllocation(props.name)}></FaMinusCircle></td>
+       <td><FaTimesCircle size='1.5em' color="red" onClick={handleDeleteExpense}></FaTimesCircle></td>
+       </tr>
+   );
 };
 
 
 
 
+
+
+
+
 export default ExpenseItem;
-
-
-
-
-
-
-
-
 
 
 
